f8ae3b9e00be3a525327451599f1cd304c89c67e
Rune Mariboe <rmariboe@users.noreply.github.com>
2018-07-09 13:37:36 +0200

    Expanded calls to GetMethod with types
    Expanded calls to GetMethod:
    
    $UnsafeNativeMethods.GetMethod('GetModuleHandle')
    $UnsafeNativeMethods.GetMethod('GetProcAddress')
    
    to include types (the latter won't work with newer .Net versions otherwise):
    
    $UnsafeNativeMethods.GetMethod('GetModuleHandle', [System.Type[]]@([System.String]))
    $UnsafeNativeMethods.GetMethod('GetProcAddress', [System.Type[]]@([System.Runtime.InteropServices.HandleRef], [System.String]))

--- Exfiltration/Invoke-TokenManipulation.ps1
+++ Exfiltration/Invoke-TokenManipulation.ps1
@@ -289,8 +289,8 @@ Blog on this script: http://clymb3r.wordpress.com/2013/11/03/powershell-and-toke
 	        Where-Object { $_.GlobalAssemblyCache -And $_.Location.Split('\\')[-1].Equals('System.dll') }
 	    $UnsafeNativeMethods = $SystemAssembly.GetType('Microsoft.Win32.UnsafeNativeMethods')
 	    # Get a reference to the GetModuleHandle and GetProcAddress methods
-	    $GetModuleHandle = $UnsafeNativeMethods.GetMethod('GetModuleHandle')
-	    $GetProcAddress = $UnsafeNativeMethods.GetMethod('GetProcAddress')
+	    $GetModuleHandle = $UnsafeNativeMethods.GetMethod('GetModuleHandle', [System.Type[]]@([System.String]))
+	    $GetProcAddress = $UnsafeNativeMethods.GetMethod('GetProcAddress', [System.Type[]]@([System.Runtime.InteropServices.HandleRef], [System.String]))
 	    # Get a handle to the module specified
 	    $Kern32Handle = $GetModuleHandle.Invoke($null, @($Module))
 	    $tmpPtr = New-Object IntPtr
